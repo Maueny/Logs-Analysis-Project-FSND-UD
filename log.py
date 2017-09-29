2fd366be9ea9f74440d93378f643568c2fbb8d3e
Derickhsm <derickhsm@gmail.com>
2017-09-29 00:19:51 -0700

    complete version

--- log.py
+++ log.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
+# coding:utf-8
 import psycopg2
+import datetime
 # Connect to an existing database
 DB_NAME = "news"
 
@@ -14,9 +16,47 @@ query3 = "select * from error_percent"
 
 
 def popular_articles():
-    db = psycopg2.Connect("dbname=news")
+    db = psycopg2.connect("dbname=news")
     c = db.cursor()
     c.execute(query1)
     rows = c.fetchall()
-    print rows
-    db.close（）
+    for i in range(len(rows)):
+        title = rows[i][0]
+        views = rows[i][1]
+        print("%s--%d" % (title, views))
+    db.close()
+
+
+def popular_authors():
+    db = psycopg2.connect("dbname=news")
+    c = db.cursor()
+    c.execute(query2)
+    rows = c.fetchall()
+    db.close()
+    for i in range(len(rows)):
+        name = rows[i][0]
+        views = rows[i][1]
+        print("%s--%d" % (name, views))
+
+
+def error_percent():
+    db = psycopg2.connect("dbname=news")
+    c = db.cursor()
+    c.execute(query3)
+    rows = c.fetchall()
+    db.close()
+    for i in range(len(rows)):
+        date = rows[i][0]
+        percent = rows[i][1]
+        print("%s--%d    %%"%(date, percent))
+
+
+if __name__ == "__main__":
+    print("The top 3 articles")
+    popular_articles()
+    print("\n")
+    print("The most popular author:")
+    popular_authors()
+    print("\n")
+    print(" which days did more than 1% of requests lead to errors:")
+    error_percent()
